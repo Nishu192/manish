1a882131b9de944472b673bbbaef54147e1c0ad3
nishu singh <enggnishu@gmail.com>
2023-04-06 12:04:47 -0400

    first commit

---
+++ src/manishcode/code12.java
@@ -0,0 +1,10 @@
+package manishcode;
+
+public class code12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/manishcode/code13.java
@@ -0,0 +1,10 @@
+package manishcode;
+
+public class code13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
